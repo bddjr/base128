d9509a26031058861cdf073ecbb6424d4bab9264
bddjr <bddjr08@outlook.com>
2025-01-17 17:07:13 +0800

    2.1.0 编码为0的字符不转义

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 Smaller than base64, only use ASCII, can run in web browser.
 
+The project was born for [vite-plugin-singlefile-compression](https://github.com/bddjr/vite-plugin-singlefile-compression).
+
 ## Setup
 
 ```
@@ -17,20 +19,16 @@ const decoded = base128.decode(eval(encodedTemplate))
 
 ---
 
-Char Code (ASCII) :
+## Effect
 
-```js
-'\0\1\2\3\4\5\6\7\b\t\n\v\f\r\x0E\x0F\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~\x7F'
-```
-
-Encode this jpg file, use base128 is 102KiB smaller than base64:
+Encode this jpg file, use base128 is `109.85 KiB` smaller than base64:
 
 ```
 screenshot-45.519.jpg
 file length: 682086
 
 encode:
-bytes length: 804860
+bytes length: 796961
 eval length: 779527
 decoded length: 682086
 equal: true
@@ -40,9 +38,3 @@ length: 909448
 ```
 
 ![](img.jpg)
-
----
-
-The project was born for [vite-plugin-singlefile-compression](https://github.com/bddjr/vite-plugin-singlefile-compression).
-
-[Unlicense](https://unlicense.org).
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export function encode(input: Uint8Array) {
-    let ii = 0, oi = 0, str: string, jstl: string;
     const out = new Uint8Array(Math.ceil(input.length / 7 * 8))
+    let ii = 0, oi = 0
     while (ii < input.length) {
         //     0        1        2        3        4        5        6        7
         // in  00000000 11111111 22222222 33333333 44444444 55555555 66666666
@@ -16,44 +16,47 @@ export function encode(input: Uint8Array) {
         /* 7 */ out[oi++] = input[ii++] & 127
     }
 
+    let str: string, jstl: string
     return {
         uint8Array: out,
         toString() {
             return str ??= new TextDecoder().decode(out)
         },
         toJSTemplateLiterals() {
-            return jstl ??= `\`${(this.toString() as string).replace(/[\r\\`]|\${|\0\d?|<\/script/g, (match) => {
-                if (match == '\r')
-                    return '\\r'
-                if (match == '</script')
-                    return '<\\/script'
-                if (match[0] == '\0') {
-                    if (match.length == 2)
-                        return '\\x00' + match[1]
-                    return '\\0'
-                }
-                return '\\' + match
-            })}\``
+            return jstl ??= `\`${(this.toString() as string).replace(
+                /[\r\\`]|\${|<\/script/g,
+                (match) => (
+                    match == '\r'
+                        ? '\\r'
+                        : match == '</script'
+                            ? '<\\/script'
+                            : '\\' + match
+                )
+            )}\``
         }
     }
 }
 
 export function decode(input: string) {
+    const out = new Uint8Array(Math.floor(input.length / 8 * 7))
     let ii = 0, oi = 0, cache: number
-    const update = () => cache = input.charCodeAt(ii++),
-        out = new Uint8Array(Math.floor(input.length / 8 * 7))
+    const next = () => (
+        (cache = input.charCodeAt(ii++)) >> 7
+            ? cache = 0 // In HTML, 0 is likely to be converted to 65533
+            : cache
+    )
     while (ii < input.length) {
         //     0        1        2        3        4        5        6        7
         // in  _0000000 _1111111 _2222222 _3333333 _4444444 _5555555 _6666666 _7777777
         // out 00000001 11111122 22222333 33334444 44455555 55666666 67777777
 
-        /* 0 */ out[oi++] = update() << 1 | update() >> 6
-        /* 1 */ out[oi++] = cache << 2 | update() >> 5
-        /* 2 */ out[oi++] = cache << 3 | update() >> 4
-        /* 3 */ out[oi++] = cache << 4 | update() >> 3
-        /* 4 */ out[oi++] = cache << 5 | update() >> 2
-        /* 5 */ out[oi++] = cache << 6 | update() >> 1
-        /* 6 */ out[oi++] = cache << 7 | update()
+        /* 0 */ out[oi++] = next() << 1 | next() >> 6
+        /* 1 */ out[oi++] = cache << 2 | next() >> 5
+        /* 2 */ out[oi++] = cache << 3 | next() >> 4
+        /* 3 */ out[oi++] = cache << 4 | next() >> 3
+        /* 4 */ out[oi++] = cache << 5 | next() >> 2
+        /* 5 */ out[oi++] = cache << 6 | next() >> 1
+        /* 6 */ out[oi++] = cache << 7 | next()
     }
     return out
 }
